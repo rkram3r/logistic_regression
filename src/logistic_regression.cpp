183e83f966d91f4bf9d3c8e12cbc2e2ecf81c330
rkram3r <skill8r@gmail.com>
2019-04-25 13:57:26 +0200

    cannot change matrixsize.

--- src/logistic_regression.cpp
+++ src/logistic_regression.cpp
@@ -1,13 +1,13 @@
 #include "logistic_regression.h"
 
-double logistic_regression::predict(double** matrix, const unsigned short nof_rows, const unsigned short nof_columns) const
+double logistic_regression::predict(double **matrix) const
 {
 	double sum = 0;
-	for (auto row_index = 0; row_index < nof_rows; row_index++)
+	for (auto row_index = 0; row_index < nof_rows_; row_index++)
 	{
-		for (auto column_index = 0; column_index < nof_columns; column_index++)
+		for (auto column_index = 0; column_index < nof_columns_; column_index++)
 		{
-			sum -= matrix[row_index][column_index] * coef_[nof_columns * row_index + column_index];
+			sum -= matrix[row_index][column_index] * coef_[nof_columns_ * row_index + column_index];
 		}
 	}
 	return 1 / (1 + pow_(euler_, sum));
--- src/logistic_regression.h
+++ src/logistic_regression.h
@@ -2,17 +2,24 @@
 
 class logistic_regression
 {
-public:
-	explicit logistic_regression(const double* coef, const double euler, double (*pow)(double, double)) :
-		coef_{ coef },
-		euler_{ euler },
-		pow_{ pow }
+  public:
+	explicit logistic_regression(
+		const double *coef,
+		const unsigned short nof_rows,
+		const unsigned short nof_columns,
+		const double euler, double (*pow)(double, double)) : coef_{coef},
+															 nof_rows_{nof_rows},
+															 nof_columns_{nof_columns},
+															 euler_{euler},
+															 pow_{pow}
 	{
 	}
-	double predict(double** matrix, unsigned short nof_rows, unsigned short nof_columns) const;
+	double predict(double **matrix) const;
 
-private:
-	const double* coef_;
+  private:
+	const double *coef_;
+	const unsigned short nof_rows_{};
+	const unsigned short nof_columns_{};
 	const double euler_{};
 	double (*pow_)(double, double);
 };
